71c7d8af9f393560b60d900127e300d8bae801a7
Sven Reintges <89526373+sven-reintges@users.noreply.github.com>
2021-11-05 13:52:49 +0000

    Added new project which will be the final project

---
+++ experimental/Stevedores/src/main/java/com/Stevedores/Stevedores/StevedoresApplication.java
@@ -0,0 +1,13 @@
+package com.Stevedores.Stevedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StevedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StevedoresApplication.class, args);
+	}
+
+}
---
+++ experimental/Stevedores/src/test/java/com/Stevedores/Stevedores/StevedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stevedores.Stevedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StevedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
