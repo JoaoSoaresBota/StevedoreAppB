53d6eee95fd963571f88ab28ba65375383c2cc2c
Sven Reintges <89526373+sven-reintges@users.noreply.github.com>
2021-12-12 19:29:33 +0000

    Created order change request controller and added 1 GET request

--- Stevedores/src/main/java/com/stevedores/stevedores/controller/OrderChangeRequestController.java
+++ Stevedores/src/main/java/com/stevedores/stevedores/controller/OrderChangeRequestController.java
@@ -1,4 +1,28 @@
 package com.stevedores.stevedores.controller;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import java.util.UUID;
 
+
+
+//TODO implement all necessary methods/calls
+
+@Slf4j
+@Api(value="/api/orderChangeRequest",produces="application/json")
+@RestController
+@RequestMapping("/api/orderChangeRequest")
 public class OrderChangeRequestController {
+
+    //Currently only echoes
+    //TODO implement
+    @GetMapping(value="/{uuid}",produces = "application/json")
+    @ApiOperation("Return an orderChangeRequest by UUID.")
+    public String FindOrderByUUID(@PathVariable UUID uuid){
+        log.info("GET: /api/orderChangeRequest/{uuid} called, uuid="+uuid);
+        return uuid.toString();
+    }
+
+
 }
