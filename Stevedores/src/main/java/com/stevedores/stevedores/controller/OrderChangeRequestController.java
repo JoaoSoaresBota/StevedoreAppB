de41e6a4806df12141424c7c87bab1c639a3be01
Sven Reintges <89526373+sven-reintges@users.noreply.github.com>
2021-12-13 14:45:40 +0000

    Added access to h2 db to api

--- Stevedores/src/main/java/com/stevedores/stevedores/controller/OrderChangeRequestController.java
+++ Stevedores/src/main/java/com/stevedores/stevedores/controller/OrderChangeRequestController.java
@@ -1,8 +1,10 @@
 package com.Stevedores.Stevedores.controller;
 
+import com.Stevedores.Stevedores.service.OrderChangeRequestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
@@ -16,12 +18,16 @@ import java.util.UUID;
 @RequestMapping("/api/orderChangeRequest")
 public class OrderChangeRequestController {
 
+    @Autowired
+    OrderChangeRequestService orderChangeRequestService;
+
     //Currently only echoes
     //TODO implement
     @GetMapping(value="/{uuid}",produces = "application/json")
     @ApiOperation("Return an orderChangeRequest by UUID.")
-    public String FindOrderByUUID(@PathVariable UUID uuid){
+    public String FindOrderByUUID(@PathVariable String uuid){
         log.info("GET: /api/orderChangeRequest/{uuid} called, uuid="+uuid);
+        orderChangeRequestService.placeOrderChangeRequest();
         return uuid.toString();
     }
 
