26b5e94f27932119ed54cb7c53118a97a47e9da4
Sven Reintges <89526373+sven-reintges@users.noreply.github.com>
2021-12-14 14:53:56 +0000

    h2 db persistent as file

---
+++ Stevedores/src/main/resources/preloaded-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO resource_stores values (1,623.4,0);
+INSERT INTO resource_stores values (2,623.4,1);
+INSERT INTO resource_stores values (3,623.4,2);
+INSERT INTO resource_stores values (4,623.4,3);
