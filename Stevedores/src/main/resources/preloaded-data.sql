0c0ba3a6dd87f3efd30b41ef69f264a881ebf147
Sven Reintges <89526373+sven-reintges@users.noreply.github.com>
2021-12-14 18:29:42 +0000

    Update preloaded-data.sql

--- Stevedores/src/main/resources/preloaded-data.sql
+++ Stevedores/src/main/resources/preloaded-data.sql
@@ -1,4 +1,5 @@
-INSERT INTO resource_stores values (1,623.4,0);
-INSERT INTO resource_stores values (2,623.4,1);
-INSERT INTO resource_stores values (3,623.4,2);
-INSERT INTO resource_stores values (4,623.4,3);
+INSERT INTO resource_store values (1,623.4,'Water');
+INSERT INTO resource_store values (2,623.4,'Waste');
+INSERT INTO resource_store values (3,623.4,'Food');
+INSERT INTO resource_store values (4,623.4,'Cargo');
+
